2743b6ac69162cd0f8a4d9b945999993b044bf30
agus Winarno <agus.winarno@hotmail.com>
2020-08-17 14:40:44 +0700

    rename projectOne to project1

---
+++ project1/project1/DetailViewController.swift
@@ -0,0 +1,42 @@
+//
+//  DetailViewController.swift
+//  project1
+//
+//  Created by agus winarno on 8/15/20.
+//  Copyright © 2020 istana code. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet var imageView: UIImageView!
+    
+    var selectedImage: String?
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // set detail view name
+        title = selectedImage
+        navigationItem.largeTitleDisplayMode = .never
+
+        // Do any additional setup after loading the view.
+        if let imageToLoad = selectedImage{
+            imageView.image = UIImage(named: imageToLoad)
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.hidesBarsOnTap = false
+    }
+    
+
+}
---
+++ project1/project1/ViewController.swift
@@ -0,0 +1,73 @@
+//
+//  ViewController.swift
+//  project1
+//
+//  Created by agus winarno on 8/15/20.
+//  Copyright © 2020 istana code. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    //varible untuk menyimpan gambar dari item
+    var pictures = [String]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        title = "Strom Viewer"
+        
+        // set large  title
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+
+        for item in items {
+            if item.hasPrefix("nssl") {
+                
+                // this is a picture to load!
+                pictures.append(item)
+                
+                
+                
+            }
+        }
+        
+        // cek print picture
+        print(pictures)
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        // return picture count to the row
+        return pictures.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        // define cell
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        
+        // set picture name to label of cell
+        cell.textLabel?.text = pictures[indexPath.row]
+        
+        // return cell
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        if let vc = storyboard?.instantiateViewController(identifier: "Detail") as? DetailViewController{
+            vc.selectedImage = pictures[indexPath.row]
+            
+            navigationController?.pushViewController(vc, animated: true)
+        }
+    }
+    
+
+}
+
