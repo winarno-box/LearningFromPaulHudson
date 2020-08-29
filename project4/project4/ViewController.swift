5ce5af2ba95a42c45fcdf4f257723f0218cde6ef
agus Winarno <agus.winarno@hotmail.com>
2020-08-29 21:16:49 +0700

    add UIAlertController on project4

--- project4/project4/ViewController.swift
+++ project4/project4/ViewController.swift
@@ -22,6 +22,9 @@ class ViewController: UIViewController, WKNavigationDelegate {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+        // add navigation in right bar
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openTapped))
+        
         let urlString = "https://www.hackingwithswift.com"
         let url = URL(string: urlString)
         let request = URLRequest(url: url!)
@@ -32,6 +35,25 @@ class ViewController: UIViewController, WKNavigationDelegate {
         // add navigation controll go back preview page
         webView.allowsBackForwardNavigationGestures = true
         
+    }
+    
+    @objc func openTapped(){
+        let ac = UIAlertController(title: "Open page…", message: nil, preferredStyle: .actionSheet)
+        ac.addAction(UIAlertAction(title: "apple.com", style: .default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "hackingwithswift.com", style: .default, handler: openPage))
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        ac.popoverPresentationController?.barButtonItem = self.navigationItem.rightBarButtonItem
+        present(ac, animated: true)
+    }
+    
+    func openPage(action: UIAlertAction) {
+        guard let actionTitle =  action.title else {return}
+        guard let url = URL(string: "https://" + actionTitle) else {return}
+        webView.load(URLRequest(url: url))
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        title = webView.title
     }
 
 
